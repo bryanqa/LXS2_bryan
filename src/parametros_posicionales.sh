7b2c2199c7e9bcd43ae4d3b0ed2b933db8317aae
root <root@Ubunto-09>
2016-10-08 09:21:53 -0600

    Inicio de el script de parametros posicionales

---
+++ src/parametros_posicionales.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+
+
+# La salida estandar de error estara redireccionada al archivo if-then-sh-.err
+
+if cp archivo1.txt archivo.txt.bck 2> $0.err
+then	
+
+
+	echo "El archivo1.txt fue correctamente respaldado" 
+fi
