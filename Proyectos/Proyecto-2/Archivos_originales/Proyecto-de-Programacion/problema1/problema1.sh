507add9edec341b62776b01f128a877a77771221
Bryan Quesada <bryanq99@gmail.com>
2016-10-11 17:07:33 -0600

    Creacion de archivos csv

---
+++ Proyectos/Proyecto-2/Archivos_originales/Proyecto-de-Programacion/problema1/problema1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DATA=./hojasDatos
+mkdir %DATA/datos_csv
+
+OUT_DATA=$DATA/archivos_csv
+
+m=0
+
+for i in `find $DATA -name ʼ*xlsʼ `
+
+do 
+	echo "Procesando archivo $i"
+
+	xls2csv $i > &OUT_DATA/data-$m.csv
+	let m=m+1
+done 2> error1.log
+
