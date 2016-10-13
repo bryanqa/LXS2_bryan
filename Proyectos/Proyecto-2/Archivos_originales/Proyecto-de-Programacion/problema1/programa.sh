49e5d32d2b021c8ab8a99e03ebdc5edf94933293
Bryan Quesada <bryanq99@gmail.com>
2016-10-13 05:20:08 -0600

    Modificacion de programa.sh para graficos(no funciona)

--- Proyectos/Proyecto-2/Archivos_originales/Proyecto-de-Programacion/problema1/programa.sh
+++ Proyectos/Proyecto-2/Archivos_originales/Proyecto-de-Programacion/problema1/programa.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DATA=./hojasDatos
-mkdir $DATA/datos_csv
-
 OUT_DATA=$DATA/datos_csv
-
 GRAF_DATA=$DATA/datos_graf
+FULL_DATA=$DATA/full_datos
+mkdir $DATA/datos_csv
 mkdir $GRAF_DATA
+mkdir $FULL_DATA
 m=0
 
 for i in `find $DATA -name "*.xls" | sort -n` 
@@ -27,5 +27,41 @@ do
 					let m=m+1
 done 2> error2.log
 
+
+#CREANDO GRAFICOS DE TODOS LOS DATOS
+
+if [ -a $FULL_DATA/full.dat ]
+then
+	rm $FULL_DATA/full.dat
+	echo "Archivo full.dat borrado"
+fi 2> errorIf.log
+
+for k in `find $GRAF_DATA -name "*.dat"`
+do	
+	sed sed '1d' $k >> $FULL_DATA/full.dat
+	echo "Procesando archivo $k"
+done 2> error3.log
+
+FMT_BEGIN="20110205 0000"
+FMT_END="20110209 0259"
+FMT_X_SHOW=%H: %M
+DATA_DONE=$FULL_DATA/full.dat
+
+graficar()
+{
+	gnuplot << EOF 2> error.log
+
+	set xdata time 
+	set timefmt '%Y%m%d %H%M
+	set xrange ["$FMT_BEGIN" . "$FMT_END" ]
+	set format x "$FMT_X_SHOW"
+	set terminal png
+	set output 'fig2.png'
+	plot "$DATA_DONE" using 1:3 with lines title "sensor1", "$DATA_DONE" using 1:4 with linespoint title "sensor2"
+EOF
+
+}
+
+graficar
 
 
