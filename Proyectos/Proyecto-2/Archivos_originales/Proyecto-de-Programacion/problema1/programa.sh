04a3e0cc30de8044cf7b222f787f4aacde2c5212
Bryan Quesada <bryanq99@gmail.com>
2016-10-14 22:41:32 -0600

    Programa.sh esta funcionando

--- Proyectos/Proyecto-2/Archivos_originales/Proyecto-de-Programacion/problema1/programa.sh
+++ Proyectos/Proyecto-2/Archivos_originales/Proyecto-de-Programacion/problema1/programa.sh
@@ -42,4 +42,26 @@ do
 	echo "Procesando archivo $k"
 done 2> error3.log
 
+FMT_BEGIN="20110205 0000"
+FMT_END="20110209 0259"
+FMT_X_SHOW=%H: %M
+DATA_DONE=$FULL_DATA/full.dat
+
+graficar()
+{
+        gnuplot << EOF 2> error.log
+
+        set xdata time 
+        set timefmt '%Y%m%d %H%M
+        set xrange ["$FMT_BEGIN" . "$FMT_END" ]
+        set format x "$FMT_X_SHOW"
+        set terminal png
+        set output 'fig2.png'
+        plot "$DATA_DONE" using 1:3 with lines title "sensor1", "$DATA_DONE" using 1:4 with linespoint title "sensor2"
+EOF
+
+}
+
+graficar
+
 
